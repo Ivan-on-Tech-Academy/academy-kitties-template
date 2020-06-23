9a40cb4f9b9d400117bab15d5c7c3bd02e035ca4
Filip Martinsson <martinsson.filip@gmail.com>
2020-06-23 17:10:18 +0200

    Update catSettings.js

--- client/assets/js/catSettings.js
+++ client/assets/js/catSettings.js
@@ -32,37 +32,6 @@ $( document ).ready(function() {
   renderCat(defaultDNA)
 });
 
-function defaultCat(){
-    renderCat(defaultDNA)
-}
-
-function randomDNA(){
-    var dnaStr = String(Math.floor(Math.random()*1E16))
-    //Colors
-    var dna = {
-    "headcolor" : dnaStr.substring(0, 2),
-    "mouthColor" : dnaStr.substring(2, 4),
-    "eyesColor" : dnaStr.substring(4, 6),
-    "earsColor" : dnaStr.substring(6, 8),
-    //Cattributes
-    "eyesShape" : dnaStr.substring(8,9) % 8 + 1    ,
-    "decorationPattern" : dnaStr.substring(9, 10)  % 8 + 1,
-    "decorationMidcolor" : dnaStr.substring(10, 12),
-    "decorationSidescolor" : dnaStr.substring(12, 14),
-    "animation" :  dnaStr.substring(14, 15) % 6 + 1,
-    "lastNum" :  dnaStr.substring(15, 16)
-    }
-
-    return dna
-}
-
-//Random cat DNA
-function randomCat(){
- var dna = randomDNA()
-    //Rendering Cat
-   renderCat(dna)
-}
-
 function getDna(){
     var dna = ''
     dna += $('#dnabody').html()
