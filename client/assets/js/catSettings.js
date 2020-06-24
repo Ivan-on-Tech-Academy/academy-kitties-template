4670eee43c685861fd4526d60e68373be1d2f1ef
Filip Martinsson <martinsson.filip@gmail.com>
2020-06-24 18:51:02 +0200

    Update catSettings.js

--- client/assets/js/catSettings.js
+++ client/assets/js/catSettings.js
@@ -42,7 +42,7 @@ function getDna(){
     dna += $('#dnadecoration').html()
     dna += $('#dnadecorationMid').html()
     dna += $('#dnadecorationSides').html()
-    dna += $('#dnadanimation').html()
+    dna += $('#dnaanimation').html()
     dna += $('#dnaspecial').html()
 
     return dna
