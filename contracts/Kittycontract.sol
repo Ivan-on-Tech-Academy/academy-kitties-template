c08fed6830fd3c1e6658263541949a5a17578345
Filip Martinsson <martinsson.filip@gmail.com>
2020-06-24 10:21:15 +0200

    added state variables

--- contracts/Kittycontract.sol
+++ contracts/Kittycontract.sol
@@ -4,7 +4,21 @@ import "./IERC721.sol";
 
 contract Kittycontract is IERC721 {
 
-    mapping(address => uint256) ownershipTokenCount;
+    string public constant name = "FilipKitties";
+    string public constant symbol = "FK";
+
+    struct Kitty {
+        uint256 genes;
+        uint64 birthTime;
+        uint32 mumId;
+        uint32 dadId;
+        uint16 generation;
+    }
+
+    Kitty[] kitties;
+
+    mapping (uint256 => address) public kittyIndexToOwner;
+    mapping (address => uint256) ownershipTokenCount;
 
     function balanceOf(address owner) external view returns (uint256 balance){
         return ownershipTokenCount[owner];
