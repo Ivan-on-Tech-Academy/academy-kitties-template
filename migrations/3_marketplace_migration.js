2fa662a24fa455c86e788ae170fff4722a78df73
Filip Martinsson <martinsson.filip@gmail.com>
2020-06-28 16:30:57 +0200

    template-for-students

--- migrations/3_marketplace_migration.js
+++
@@ -1,7 +0,0 @@
-const Marketplace = artifacts.require("KittyMarketPlace");
-const Kittycontract = artifacts.require("Kittycontract");
-
-module.exports = function(deployer) {
-  console.log(Kittycontract.address)
-  deployer.deploy(Marketplace, Kittycontract.address);
-};
