148f9bebff943dbfa8a1f0ceaf52740b12ade77c
Filip Martinsson <martinsson.filip@gmail.com>
2020-10-01 14:35:21 +0200

    updated constr

--- contracts/Kittycontract.sol
+++ contracts/Kittycontract.sol
@@ -61,6 +61,11 @@ contract Kittycontract is IERC721, Ownable {
 
     uint256 public gen0Counter;
 
+    constructor() public {
+        // We are creating the first kitty at index 0  
+        _createKitty(0, 0, 0, uint256(-1), address(0));
+    }
+
     function breed(uint256 _dadId, uint256 _mumId) public returns (uint256){
         require(_owns(msg.sender, _dadId), "The user doesn't own the token");
         require(_owns(msg.sender, _mumId), "The user doesn't own the token");
